bf093e15c91633d835f9418ea99012eb31d3540e
Albert Rapp <AlbertRapp@gmx.de>
2025-05-19 16:45:52 +0200

    Add optional transcript and change everything from musevideo to muse-video

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# muse-video
+# MuseVideo Extension For Quarto
+
+This extension allows Quarto users to integrate videos (and their transcription) from [Muse AI](https://muse.ai/) using a simple shortcode.
+
+## Installing
+
+
+```bash
+quarto add rfortherestofus/muse-video
+```
+
+This will install the extension under the `_extensions` subdirectory.
+If you're using version control, you will want to check in this directory.
+
+## Using
+
+> [!IMPORTANT]
+> The same video (by video-ID) cannot be inserted into the same Quarto page.
+
+Inside your Quarto file, use the shortcode:
+
+```
+{{ muse-video <video_id> }}
+```
+
+And if you want to have the transcripts as well, you can use: 
+
+```
+{{ muse-video <video_id> with_transcript=true }}
+```
+
+
+## Example
+
+Here is the source code for a minimal example: [example.qmd](example.qmd).
+
---
+++ _extensions/muse-video/muse-video.lua
@@ -0,0 +1,194 @@
+return {
+  ['muse-video'] = function(args, kwargs, meta, raw_args, context) 
+    
+    local video_id = args[1]
+    local with_transcript = pandoc.utils.stringify(kwargs["with_transcript"])
+    local html_transcript_content
+
+    if with_transcript == 'true' then
+      html_transcript_content = string.format([[
+        <details class="container-transcript" style="margin-top:20px;font-size:larger;">
+          <summary>Transcript</summary>
+          <div id="transcript-list-%s" style="height:200px; overflow-y:scroll;">
+              <table><tbody id="speech-container-%s"></tbody></table>
+          </div>
+        </details>
+
+        <style>
+          .current {
+            background: black;
+            color: white;
+            border-radius: 1px;
+          }
+        </style>
+
+        <script>
+          // Highlight the current word.
+          player_%s.on('timeupdate', function(time) {
+            document.querySelectorAll('#transcript-list-%s .word').forEach(function(word) {
+              if (Number.parseFloat(word.dataset.start) <= time && Number.parseFloat(word.dataset.end) > time) {
+                word.classList.add('current');
+                word.scrollIntoView({behavior: "smooth", block: "center", inline: "end"});
+              } else if (word.classList.contains('current')) {
+                word.classList.remove('current');
+              }
+            });
+          });
+          // Fetch words and place them insde container-transcript as phrases.
+          fetch(
+            'https://muse.ai/api/files/i/speech/' + svid_%s, {mode: 'cors'}
+          )
+          .then(function(response) {
+            return response.json();
+          })
+          .then(function(words) {
+            // Sort words by time.
+            words.sort((a, b) => a[0] - b[0]);
+            // Group into phrases.
+            let phrases = [];
+            let wordPrev;
+            words.forEach(word => {
+              let paragraphStart = word[2] == '\n' || wordPrev && word[0] - wordPrev[1] > 1.2;
+              if (phrases.length == 0 || paragraphStart && wordPrev[2].trim()) {
+                phrases.push([]);
+              }
+              if (word[2].trim()) {
+                phrases[phrases.length - 1].push(word);
+              }
+              wordPrev = word;
+            });
+            // Draw phrases.
+            let list = document.getElementById('speech-container-%s');
+            phrases.forEach(phrase => {
+              if (!phrase.length) {
+                return;
+              }
+              let time = phrase[0][0];
+              let timeLen = time > 3600 ? 8 : (time > 600 ? 5 : 4);
+              let timeStart = new Date(time * 1000).toISOString().substr(19 - timeLen, timeLen);
+              let wordsHTML = '';
+              phrase.forEach(word => {
+                const punctJoin = ';:.,'.includes(word[2]);
+                const classes = 'word' + (punctJoin ? ' punct-join' : '') + ('-–'.includes(word[2]) ? ' punct-space' : '');
+                const space = punctJoin ? '' : ' ';
+                wordsHTML += `${space}<span class="${classes}" data-start="${word[0]}" data-end="${word[1]}">${word[2]}</span>`;
+              });
+              list.insertAdjacentHTML('beforeend', `
+              <tr>
+                <td class="phrase-time">${timeStart}</td>
+                <td class="phrase">${wordsHTML}</td>
+              </tr>
+              `);
+            });
+          });
+          // On word click/touch, seek video to where the word is spoken.
+          document.addEventListener('click', e => {
+            const speechContainer = e.target.closest('#speech-container-%s');
+            if (speechContainer && e.target.classList.contains('word')) {
+              player_%s.seek(Number.parseFloat(e.target.dataset.start));
+              player_%s.play();
+            }
+          });
+
+          let isScrollActive_%s = false;
+          const onScroll_%s = () => {
+            const containerVideo = document.getElementById('transcript-list-%s');
+            const scroll = containerVideo.scrollTop;
+            if (scroll > 70) {
+              containerVideo.classList.add('scrolled');
+            } else {
+              containerVideo.classList.remove('scrolled');
+            }
+          }
+          // Enable scroll listener when playing
+          player_%s.on('Play', () => {
+            if (!isScrollActive_%s) {
+              window.addEventListener('scroll', onScroll_%s);
+              isScrollActive_%s = true;
+            }
+          });
+          // Optional: Disable scroll listener when paused/stopped
+          player_%s.on('Pause', () => {
+            if (isScrollActive_%s) {
+              window.removeEventListener('scroll', onScroll_%s);
+              isScrollActive_%s = false;
+            }
+          });
+
+          player_%s.on('Ended', () => {
+            if (isScrollActive_%s) {
+              window.removeEventListener('scroll', onScroll_%s);
+              isScrollActive_%s = false;
+            }
+          });
+        </script>
+      ]], 
+        video_id,
+        video_id,
+        video_id,
+        video_id,
+        video_id,
+        video_id,
+        video_id,
+        video_id,
+        video_id,
+        video_id,
+        video_id,
+        video_id,
+        video_id,
+        video_id,
+        video_id,
+        video_id,
+        video_id,
+        video_id,
+        video_id,
+        video_id,
+        video_id,
+        video_id,
+        video_id,
+        video_id,
+        video_id,
+        video_id,
+        video_id,
+        video_id
+      )
+    end
+    
+    local html_video_player = string.format([[
+      <div id="container-video-%s" class="container">
+          <div id="video-player-%s"></div>
+      </div>
+
+      <script src="https://muse.ai/static/js/embed-player.min.js"></script>
+      <script>
+        // Load player with video id into #video-player.
+        const svid_%s = '%s';
+        const player_%s = MusePlayer({
+          container: '#video-player-%s',
+          video: svid_%s,
+          width: 'parent',
+          showTitle: false,
+        });
+      </script>
+      
+    ]],
+     video_id,
+     video_id,
+     video_id,
+     video_id,
+     video_id,
+     video_id,
+     video_id          
+    )
+    
+    if with_transcript == 'true' then
+      return {
+        pandoc.RawBlock('html', html_video_player),
+        pandoc.RawBlock('html', html_transcript_content)
+      }
+    else 
+      return pandoc.RawBlock('html', html_video_player)
+    end
+
+  end
+}
